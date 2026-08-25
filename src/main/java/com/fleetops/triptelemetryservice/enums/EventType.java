1a07516e3ad77206b3c54dd4164afb662416180d
Chanuth Dewhan <chanuthdewhan273@gmail.com>
2026-08-25 13:33:32 +0530

    feat: add trip telemetry enums

---
+++ src/main/java/com/fleetops/triptelemetryservice/enums/EventType.java
@@ -0,0 +1,8 @@
+package com.fleetops.triptelemetryservice.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum EventType {
+    LOCATION, STATUS_CHANGE
+}
---
+++ src/main/java/com/fleetops/triptelemetryservice/enums/TripStatus.java
@@ -0,0 +1,8 @@
+package com.fleetops.triptelemetryservice.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum TripStatus {
+    STARTED, IN_PROGRESS, COMPLETED, CANCELLED
+}
